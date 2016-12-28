e62a4a4e130a32076a6e542e5ea1c5a852aebc2e
r4k0nb4k0n <chj6097@gmail.com>
2016-12-28 11:36:40 +0900

    Update 1152.cpp

--- 1152.cpp
+++ 1152.cpp
@@ -5,33 +5,33 @@ using namespace std;
 
 int main()
 {
-	char s[1000001];
-	gets(s);
-	int cnt = 0;
-	int idx = 0;
+	char s[1000001]; // maximum length : 1,000,000
+	gets(s); // including space
+	int cnt = 0; // the number of vocabularies
+	int idx = 0; // index
 	bool now_voca = false;
-	if(s[idx] != ' ')
+	if (s[idx] != ' ')
 		now_voca = true;
-	while(s[idx] != '\0')
+	while (s[idx] != '\0')
 	{
-		if(s[idx] == ' ')
+		if (s[idx] == ' ') // space
 		{
-			if(now_voca == true)
+			if (now_voca == true)
 			{
 				cnt++;
 				now_voca = false;
 			}
 			idx++;
 		}
-		else
+		else // not space
 		{
 			now_voca = true;
 			idx++;
 		}
 	}
-	if(s[idx-1] != ' ')
-		printf("%d",cnt+1);
-	else
-		printf("%d",cnt);
+	if (s[idx - 1] != ' ') // last is not space
+		printf("%d", cnt + 1);
+	else // last is space
+		printf("%d", cnt);
 	return 0;
 }
