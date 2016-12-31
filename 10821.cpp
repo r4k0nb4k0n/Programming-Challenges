e6aba92904b02f998f1e90a4a1a0f5819d1ac2cc
r4k0nb4k0n <chj6097@gmail.com>
2016-12-31 13:35:16 +0900

    Update 10821.cpp

--- 10821.cpp
+++ 10821.cpp
@@ -1,18 +1,11 @@
 #include <cstdio>
-using namespace std;
-char s[101];
-int idx = 0;
-int cnt = 0;
-
 int main()
 {
-	scanf("%s", s);
-	while (s[idx] != '\0')
-	{
-		if (s[idx] == ',')
+	char c;
+	int cnt = 0;
+	while ((c = getchar()) && c != '\n')
+		if (c == ',')
 			cnt++;
-		idx++;
-	}
 	printf("%d", cnt + 1);
 	return 0;
 }
