842e2efb6cf0f200573f9130fe7ec407f27960ff
r4k0nb4k0n <chj6097@gmail.com>
2016-12-31 13:26:44 +0900

    Update 2743.cpp

--- 2743.cpp
+++ 2743.cpp
@@ -1,14 +1,11 @@
 #include <cstdio>
-
-using namespace std;
-
 int main()
 {
-    char s[100];
-    scanf("%s",s);
-    int i=0;
-    while(s[i] != '\0')
-        i++;
-    printf("%d",i);
-    return 0;
+	char s[100];
+	scanf("%s", s);
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	printf("%d", i);
+	return 0;
 }
