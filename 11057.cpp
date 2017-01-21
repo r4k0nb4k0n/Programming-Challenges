a31b7f0c1b6bb3cabf8f0222637a7a844998e610
r4k0nb4k0n <chj6097@gmail.com>
2017-01-21 10:46:52 +0900

    Update 11057.cpp

--- 11057.cpp
+++ 11057.cpp
@@ -1,31 +1,26 @@
 #include <cstdio>
-#include <string.h>
+#include <cstring>
 #define MOD 10007
-
-using namespace std;
 
 int N; // 길이 1 <= N <= 1001인 자연수.
 int memo[10][1001];
-int solve(int X,int n) // n자리일 때 오름차 수 개수 리턴.
+int solve(int X,int n) // 함수의 정의 : n자리일 때 오르막 수 개수 리턴.
 {
 	// 기저조건.
-	if( n==1 && !(X<0) )
-		return 1;
 	if(X<0 || n<0)
 		return 0;
+    if(n==1 && !(X<0)) // 그동안 조건 만족하면서 끝까지 도달
+		return 1;
 
-	int &ret = memo[X][n];
+	int &ret = memo[X][n]; // 주소 일치
 
 	if( ret != 0 )
 		return ret;
 
 	for(int i=0;i<=X;i++)
-	{
-		ret = ret + solve(i,n-1)%MOD;
-	}
+		ret = ret + solve(i,n-1)%MOD; // 점화식
 
 	return ret%MOD;
-
 }	
 
 int main(void)
