7c6cfdb460e30a0e22c96c24a267d71c5f708222
r4k0nb4k0n <chj6097@gmail.com>
2017-07-18 14:40:23 +0900

    Update 12353.cpp

--- 12353.cpp
+++ 12353.cpp
@@ -10,20 +10,20 @@ int main() {
 		getchar();
 		scanf("%c", &gender);
 		scanf("%d'%d\"",&feet,&inch);
-		mother = inch + (feet * 12);
+		mother = inch + (feet * 12); // 인치로 변환
 		scanf("%d'%d\"", &feet, &inch);
-		father = inch + (feet * 12);
+		father = inch + (feet * 12); // 인치로 변환
 		bool sw;
-		if ((father + mother) % 2 == 0) sw = false;
-		else sw = true;
+		if ((father + mother) % 2 == 0) sw = false; // (아빠 키 + 엄마 키) 짝수일 때
+		else sw = true; // (아빠 키 + 엄마 키) 홀수일 때
 		double midf = (double)(father + mother);
-		if (gender == 'B') midf += 5.0;
-		else midf -= 5.0;
+		if (gender == 'B') midf += 5.0; // 남자아이는 + 5인치
+		else midf -= 5.0; // 여자아이는 - 5인치
 		midf /= (double)2;
 		int mid = (int)midf;
-		if(sw)
+		if(sw) // 합이 짝수일 때 그대로 출력
 			printf("Case #%d: %d'%d\" to %d'%d\"\n", x, (mid - 4) / 12, (mid - 4) % 12, (mid + 4) / 12, (mid + 4) % 12);
-		else
+		else // 합이 홀수일 때 최소값에 1인치를 더해주고 출력
 			printf("Case #%d: %d'%d\" to %d'%d\"\n", x, (mid - 3) / 12, (mid - 3) % 12, (mid + 4) / 12, (mid + 4) % 12);
 	}
 	return 0;
