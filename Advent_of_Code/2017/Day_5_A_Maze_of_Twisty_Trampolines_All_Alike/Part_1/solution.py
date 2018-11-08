93b2973db3c05b81a5dd3b29d5f0476cef8beb83
Hyeong Jin Choe <chj6097@gmail.com>
2018-11-08 00:56:43 +0000

    Solve Advent of Code 2017 Day 5 A Maze of Twisty Trampolines All Alike

---
+++ Advent_of_Code/2017/Day_5_A_Maze_of_Twisty_Trampolines_All_Alike/Part_1/solution.py
@@ -0,0 +1,15 @@
+instructions = []
+with open('input.txt', 'r') as f:
+	for line in f:
+		instructions.append(int(line))
+	
+prev = 0
+pc = 0
+steps = 0
+while pc in range(len(instructions)):
+	prev = pc
+	pc += instructions[pc]
+	instructions[prev] += 1
+	steps += 1
+	
+print("FIND THE EXIT OF THE MAZE!!! " + str(steps) + " has taken.")
